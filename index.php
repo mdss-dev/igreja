8a556050cb02863b954eabeece4d6d85f36edbab
Michael Douglas <douglas.ti.dev@gmail.com>
2023-12-17 15:28:46 -0300

    app funcionando com configurações básicas

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once("conexao.php");
+ ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+  <title>ICO</title>
+</head>
+<body>
+  <h1><?php echo($nome_igreja)?></h1>
+</body>
+</html>
