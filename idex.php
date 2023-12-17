0a3247d46072b4b4228321abba3820df3141063f
Michael Douglas <douglas.ti.dev@gmail.com>
2023-12-17 10:55:51 -0300

    projeto criado

---
+++ idex.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ICO</title>
+</head>
+<body>
+  <h1>ICO - Felicidade</h1>
+</body>
+</html>
